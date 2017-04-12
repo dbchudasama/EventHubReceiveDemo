dc589c01e9a2adbde9148c8f623f811b5173c7c3
Divyesh B Chudasama <dbchudasama@gmail.com>
2017-04-12 17:39:11 +0100

    EventHub Receive Event Demo Program

---
+++ receiveTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System.Threading.Tasks;
+
+namespace receiveTest
+{
+    public class Program
+    {
+        //Setting the connection credentials in order to be able to connect to the Azure Eventhub
+        private const string EhConnectionString = "Endpoint=sb://eventhubdemodiv.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=FveFVfhwjuVuhOYO9qMYx8OejbiuSzIUt95ZGCZkl94=";
+        private const string EhEntityPath = "eventhubdemodiv";
+        private const string StorageContainerName = "demoblob";
+        private const string StorageAccountName = "mystorageaccountsuk";
+        private const string StorageAccountKey = "AyDXAIoBFndHMGY+of31fBLXSnfmor2v2xEal4V3iwxx52wdbBrt/WDz+IMU0IC8Bbjl0dfW98ACKobdiA0Yjg==";
+
+        private static readonly string StorageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", StorageAccountName, StorageAccountKey);
+
+        //Main Method
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Registering EventProcessor...");
+
+            var eventProcessorHost = new EventProcessorHost(
+                EhEntityPath,
+                PartitionReceiver.DefaultConsumerGroupName,
+                EhConnectionString,
+                StorageConnectionString,
+                StorageContainerName);
+
+            // Registers the Event Processor Host and starts receiving messages
+            await eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>();
+
+            Console.WriteLine("Receiving. Press ENTER to stop worker.");
+            Console.ReadLine();
+
+            // Disposes of the Event Processor Host
+            await eventProcessorHost.UnregisterEventProcessorAsync();
+        }
+    }
+}
---
+++ receiveTest/SimpleEventProcessor.cs
@@ -0,0 +1,48 @@
+﻿
+namespace receiveTest
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.EventHubs;
+    using Microsoft.Azure.EventHubs.Processor;
+
+    //Event Processor Class
+    public class SimpleEventProcessor : IEventProcessor
+    {
+
+        //Task to close processor
+        public Task CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.WriteLine($"Processor Shutting Down. Partition '{context.PartitionId}', Reason: '{reason}'.");
+            return Task.CompletedTask;
+        }
+
+        //Task to Open Processor
+        public Task OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine($"SimpleEventProcessor initialized. Partition: '{context.PartitionId}'");
+            return Task.CompletedTask;
+        }
+
+        //Task to display error message if cannot connect to partition
+        public Task ProcessErrorAsync(PartitionContext context, Exception error)
+        {
+            Console.WriteLine($"Error on Partition: {context.PartitionId}, Error: {error.Message}");
+            return Task.CompletedTask;
+        }
+
+        //Finally, successfull connection, hence receiving event details.
+        public Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach (var eventData in messages)
+            {
+                var data = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+                Console.WriteLine($"Message received. Partition: '{context.PartitionId}', Data: '{data}'");
+            }
+
+            return context.CheckpointAsync();
+        }
+    }
+}
